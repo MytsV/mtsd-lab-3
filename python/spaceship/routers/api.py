1a1733f41b2580efc5c783ab2177cab74da69a01
MytsV <mytsvictoria@gmail.com>
2023-04-03 00:10:53 +0300

    Add matrix multiplication. Remove unused file
    Backend.in is no longer needed

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -6,3 +6,20 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/product')
+def matrix_product() -> dict:
+    import numpy as np
+
+    # Create two 10x10 random matrices
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+
+    # Multiply the matrices together
+    result = np.dot(matrix_a, matrix_b)
+
+    return {
+        'matrix_a': matrix_a.tolist(), 
+        'matrix_b': matrix_b.tolist(), 
+        'product': result.tolist()
+    }
